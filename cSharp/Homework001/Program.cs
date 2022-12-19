bd8ebb5c97ba68c407bae8c22ffc153cdb6c7627
SantimosHub <119481932+SantimosHub@users.noreply.github.com>
2022-12-19 12:45:26 +0300

    Delete Program.cs

--- cSharp/Homework001/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
-int result = number * number;
-
-Console.WriteLine("Квадрат числа " + number + " будет " + result);
