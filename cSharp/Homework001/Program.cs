656380f2253b695116101970c5cb024063759454
Sant <stosoleg@gmail.com>
2022-12-19 10:09:33 +0300

    programm

--- cSharp/Homework001/Program.cs
+++ cSharp/Homework001/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+
+Console.WriteLine("Квадрат числа " + number + " будет " + result);
